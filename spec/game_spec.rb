e62c98f8198ce8f0b266d7ff84ae80775356074b
Anthony Cook <anthonymichaelcook@gmail.com>
2011-09-09 15:16:55 -0300

    The lack of pluralization was annoying me.

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require './game'
 
 describe Game do
   describe "#score" do
-    it "returns 0 for all gutter game" do
+    it "returns 0 for all gutter games" do
       game = Game.new
       20.times { game.roll(0) }
       game.score.should == 0
